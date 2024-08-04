0eaed50c54ae004d9d9882315351d0f5f113aa8a
sree085 <sreedevsv085@gmail.com>
2024-08-04 13:59:32 +0530

    first

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //Write missing code here
 mongoose
-  .connect('mongodb+srv://ArwinRolant:Godfather514@cluster0.daxgwvt.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0')
+  .connect('mongodb+srv://vaigunth1:vaigu1125@cluster0.zedjtrz.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
     console.log("Connected to DB");
   })
